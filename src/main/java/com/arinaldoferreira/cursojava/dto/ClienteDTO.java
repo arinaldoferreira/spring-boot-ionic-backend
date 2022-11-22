b47d02300d32566c72e03451f80253d0afbec840
Arinaldo Ferreira <arinaldo.ferreira@outlook.com>
2022-11-22 20:31:27 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/arinaldoferreira/cursojava/dto/ClienteDTO.java
+++ src/main/java/com/arinaldoferreira/cursojava/dto/ClienteDTO.java
@@ -2,28 +2,34 @@ package com.arinaldoferreira.cursojava.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.arinaldoferreira.cursojava.domain.Categoria;
+import com.arinaldoferreira.cursojava.domain.Cliente;
 
-public class CategoriaDTO implements Serializable {
+public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 
-	public CategoriaDTO() {
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")	
+	private String email;
+
+	public ClienteDTO() {
 
 	}
 
-	public CategoriaDTO(Categoria obj) {
+	public ClienteDTO(Cliente obj) {
 		this.id = obj.getId();
 		this.nome = obj.getNome();
+		this.email = obj.getEmail();
 	}
 
 	public Integer getId() {
@@ -41,4 +47,13 @@ public class CategoriaDTO implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
