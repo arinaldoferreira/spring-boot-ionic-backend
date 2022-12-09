5f004b12d56e45b6340400d82ed64e190cd91f07
Arinaldo Ferreira <arinaldo.ferreira@outlook.com>
2022-12-08 21:33:18 -0300

    Busca de produtos por nome e categoria

---
+++ src/main/java/com/arinaldoferreira/cursojava/dto/ProdutoDTO.java
@@ -0,0 +1,55 @@
+package com.arinaldoferreira.cursojava.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.arinaldoferreira.cursojava.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+
+	private Double preco;
+
+	public ProdutoDTO() {
+
+	}
+
+	public ProdutoDTO(Produto obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.preco = obj.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
