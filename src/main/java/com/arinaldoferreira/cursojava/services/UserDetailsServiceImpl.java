c78f86781781f474f59bda4af5f30f506944002e
Arinaldo Ferreira <arinaldo.ferreira@outlook.com>
2023-01-11 10:52:04 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/arinaldoferreira/cursojava/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.arinaldoferreira.cursojava.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.arinaldoferreira.cursojava.domain.Cliente;
+import com.arinaldoferreira.cursojava.repositories.ClienteRepository;
+import com.arinaldoferreira.cursojava.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
