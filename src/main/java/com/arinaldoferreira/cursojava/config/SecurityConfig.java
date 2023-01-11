ff6d94be925f0febd9f73ff93d24f6cac8387825
Arinaldo Ferreira <arinaldo.ferreira@outlook.com>
2023-01-11 12:04:32 -0300

    Implementando a autorização

--- src/main/java/com/arinaldoferreira/cursojava/config/SecurityConfig.java
+++ src/main/java/com/arinaldoferreira/cursojava/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.arinaldoferreira.cursojava.security.JWTAuthenticationFilter;
+import com.arinaldoferreira.cursojava.security.JWTAuthorizationFilter;
 import com.arinaldoferreira.cursojava.security.JWTUtil;
 
 @SuppressWarnings("deprecation")
@@ -60,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
